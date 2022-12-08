f0c1615ba7f577ebcd5dccd3874e61f33bcddb8e
dinta-coder <108183187+dinta-coder@users.noreply.github.com>
2022-12-09 00:56:48 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,11 @@
 */
 int main(void)
 {
+int a;
+long into b;
+long long into;
+char d;
+float f;
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
