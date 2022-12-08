a39352915570dedef3998c0a1f9871041a02a901
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-08 11:14:13 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry poiny
+ * returns 0
+ * a printf program
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
+
