5198f33725329833c123aa8c0453c06809672baf
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-13 09:22:31 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
- * int _isalpha - retunrns 1 if upper or lower
+ * _isalpha - retunrns 1 if upper or lower
  * @c: The character in ASCII code
  * main - comfirms the function
  * Return: Always 0
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
