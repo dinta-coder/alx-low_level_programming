ff29832af8daff5e00d555c7385b9f57caf0e2c0
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-13 15:26:02 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - check the code.
+ * print_sign - function for greater than
+ * Return: Always 0.
+ * @n: the int to be checked
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	]
+	return (1);
+	_putchar('\n');
+
+	else if (n == 0)
+	{
+		_putchar(48);
+	}
+	return (0);
+	_putchar('\n');
+
+	else
+	{
+		_putchar('-');
+	}
+}
