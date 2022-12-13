4f8dad4a720c2e0ba88bbc96479dd62efa6b831f
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-13 08:24:47 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,22 +4,20 @@
  * main - print lowercase alphabets
  * Return: Always 0
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void){
+	int i = 0;
+	char c;
 
-{
-        int i = 0;
-        char c;
-
-        while (i <10)
-        {
-        	 c = 'a';
-        	while (c <= 'z')
-        	{
-        		_putchar(c);
-        		c++;
-        	}
-        	i++;
-               	_putchar('\n');
+	while (i < 10)
+	{
+		c = 'a'
+		while (c <= z)
+		{
+			_putchar(c);
+			c++;
 		}
+		i++;
+		putchar('\n');
+	}
 }
 
