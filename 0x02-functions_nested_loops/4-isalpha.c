7be3945250f86fd78c2a95914e20483686f39e9b
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-13 09:06:59 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * int _isalpha - retunrns 1 if upper or lower
+ * main - comfirms the function
+ * Return: Always 0
+ */
+int _isalpha(int c);
+{
+	c = 97;
+
+	if ((c >= 97 && c <= 122) || (c >=65 && c <= 90))
+	{
+		return (1);
+	}
+
+	else
+	
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
