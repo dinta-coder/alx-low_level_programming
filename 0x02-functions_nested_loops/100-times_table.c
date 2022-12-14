91ab48b62d6a5f38955a984e8159e71572dcbcff
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-14 09:42:48 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,28 +1,49 @@
 #include "main.h"
-/**
- * main - prints all the possible different combinations of two digits
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	int a, b;
 
-	for (a = 48; a <= 56; a++)
+/**
+ * print_times_table - prints the n times table, starting from 0
+ * @n: number of times table
+ */
+void print_times_table(int n)
+{
+	int a;
+	int b;
+	int c;
+
+	if (n >= 0 && n <= 15)
 	{
-		for (b = 49; b <= 57; b++)
+		for (a = 0; a <= n; a++)
 		{
-			if (b > a)
+			for (b = 0; b <= n; b++)
 			{
-				_putchar(a);
-				_putchar(b);
-				if (a != 56 || b != 57)
+				c = b * a;
+				if (b == 0)
+				{
+					_putchar(c + '0');
+				} else if (c < 10 && b != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				} else if (c >= 10 && c < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				} else if (c >= 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
 				}
 			}
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
-	return (0);
 }
