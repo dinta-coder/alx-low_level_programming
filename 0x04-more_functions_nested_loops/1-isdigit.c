22a685ae3159990fa8e8decbd268d1bd4f05ba9e
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-15 10:46:46 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,17 +9,12 @@
 
 int _isdigit(int c)
 {
-	c = 0;
-
-	while (c <= 9; c++)
+	if (c >= 48 && c <= 57)
 	{
-		if (c >= 48 && c <= 57)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-        }
+		return (1);
+	}
+	else
+	{
+		return (0);
 }
 
