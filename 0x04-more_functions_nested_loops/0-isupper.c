3c1e25238ed107c893d06e4e83beeecadba123a4
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-15 10:01:01 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isupper - This function differenciates
+ * @c:returns 1 if isupper
+ * Return: either 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c));
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
