34ea3d3e47fb5d22a197aa59315b3930ec1aefbd
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-15 11:00:13 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - calls two parameters that are multiplied
+ * @a: first parameter
+ * @b: second parameter
+ * Return: first parameter * second parameter
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+	return (c);
+}
+
