80e643a0a1eee85ae534db290ef1c029faa261ec
dinta-coder <108183187+dinta-coder@users.noreply.github.com>
2022-12-16 19:05:03 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,27 +1,22 @@
+
 #include "main.h"
-<<<<<<< HEAD
 #include <stdio.h>
-=======
-#include <studio.h>
->>>>>>> 671a5e09531edce164e4b976f38e058107d1ad84
 
 /**
- * print_numbers - Print numbers between 0 to 9 incl.
+ * print_most_numbers - Print numbers between 0 to 9 incl.
  *
  * Return: Void.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-<<<<<<< HEAD
-		_putchar(i + '0');
-=======
-		_putchar(ch + 'O');
->>>>>>> 671a5e09531edce164e4b976f38e058107d1ad84
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
-
