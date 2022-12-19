d8d1b5d76a3b0264749f04e2a6cacb9be61902b0
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-19 07:07:21 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+"include <string.h>
+
+/**
+ * rev_string - function to reverse string.
+ * @s: parameter to be worked on.
+ * Returns: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		char revString = s[i];
+	}
+	return (revString);
+}
+
+ 
