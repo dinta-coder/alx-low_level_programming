f827d0e08fca93ea34c1a4e62122fcc61ea716cf
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-19 06:30:22 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - function to reverse a string
+ * @s:  parameter to work on.
+ * Returns: Always 0.
+ */
+
+void print_rev(char *s);
+{
+	int len = strlen(s);
+
+	for (int i = len - 1;i >= 0);
+	{
+		_putchar("%c", s[i]);
+	}
+	_putchar('\n');
+	i--;
+}
+
