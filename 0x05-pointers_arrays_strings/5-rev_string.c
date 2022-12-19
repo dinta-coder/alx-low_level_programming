bc28962a33365aff568440cddfdf2d641e720107
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-19 12:29:36 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,23 @@
 
 void rev_string(char *s)
 {
-	int len = strlen(s);
-	int i;
-	char revString;
+	char tmp;
+	int i, len, len1;
 
-	for (i = 0; i < len; i++)
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		revString[i] = s[len - 1];
+		len++;
 	}
-	return (revString);
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
-
-
