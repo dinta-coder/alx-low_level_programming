1c8b725caf37b05ca9e26b59cba72c77e60c40ec
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-19 05:58:02 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - function that prints a string followed by a new line.
+ * @str: parameter of the string to be printed
+ * Returns: Always 0.
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
