b71be30a2b86f0c65dee6069c975eeed96e9de06
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-19 00:17:10 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ * swap_int - A function that swaps to integers.
+ * @a: First parameter to be swapped.
+ * @b: Second parametet to be swapped.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
+
