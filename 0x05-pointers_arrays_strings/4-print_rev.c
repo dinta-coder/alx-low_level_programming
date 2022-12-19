6c0ab14d665c2662adba2d0daf13e441defb81f7
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-19 06:35:19 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,10 @@ void print_rev(char *s);
 {
 	int len = strlen(s);
 
-	for (int i = len - 1;i >= 0)
+	for (int i = len - 1; i >= 0; i--)
 	{
 		_putchar("%c", s[i]);
 	}
 	_putchar('\n');
-	i--;
 }
 
