fc8532b715f28fa6050be7d3682122868a25afd4
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-18 23:53:59 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that calls the updated value
+ * @*n: A pointer parameter that substitute for the updated argument.
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
+
