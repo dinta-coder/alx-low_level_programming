4538e4d418689f64117480393330ad0bfde82433
Jacob Uche <jacob.uche.100@gmail.com>
2022-12-19 06:51:14 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,18 @@
 #include "main.h"
 #include <string.h>
 
+
 /**
  * print_rev - function to reverse a string
  * @s:  parameter to work on.
  * Returns: Always 0.
  */
 
+
 void print_rev(char *s)
 {
 	int len = strlen(s);
-        int i;
+	int i;
 
 	for (i = len - 1; i >= 0; i--)
 	{
